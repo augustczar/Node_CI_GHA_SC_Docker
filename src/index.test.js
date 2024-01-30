0701df844b9ee310c35d185154207689445e99bc
augustczar <augustczarramos@gmail.com>
2024-01-30 12:07:00 -0300

    feat: code tests update

--- src/index.test.js
+++ src/index.test.js
@@ -1,12 +1,12 @@
-const { index, sum, sub, times, div } = require('./index');
+const { applyDiscount, sum, sub, times, div } = require('./index');
 
 test('Apply Discount', () => {
-    const result = index.applyDiscount(10, 5);
+    const result = applyDiscount(10, 5);
     expect(result).toEqual(5);
 });
 
 test('Apply big discount', () => {
-    const result = index.applyDiscount(5, 10);
+    const result = applyDiscount(5, 10);
     expect(result).toEqual(0);
 });
 
